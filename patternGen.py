e073a61de3d00b5eba1a3792086940d4645f4b49
Jamie Kennedy <jamiekennedy97@gmail.com>
2019-02-09 14:13:40 +0000

    Created patternGen.py and patternDicts json files
    Created patternGen.py to generate pattern dictionaries from the word dictionaries and created a json file for each pattern dict

---
+++ patternGen.py
@@ -0,0 +1,62 @@
+import os
+import json
+
+
+def main(dirName):
+    files = getFiles(dirName)
+    for file in files:
+        words = readFile("dicts/" + file)
+        patterns = {}
+
+        for word in words:
+            pattern = getPattern(word)
+            if pattern not in patterns:
+                patterns[pattern] = [word]
+            else:
+                patterns[pattern].append(word)
+
+        writeJson(file, patterns)
+
+
+def writeJson(fileName, data):
+    file = open("jsonDicts/" + [fileName.split(".")][0][0] + ".json", "w")
+    file.write(json.dumps(data))
+    file.close()
+
+
+def readFile(fileName):
+    with open(fileName) as file:
+        lines = [line.rstrip() for line in file]
+
+    return lines
+
+
+def getFiles(directory):
+    directory = os.fsencode(directory)
+    files = []
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".txt"):
+            files.append(filename)
+
+    return files
+
+
+def getPattern(word):
+    seenLetter = ""
+    pattern = ""
+    count = 0
+
+    for char in word:
+        if char not in seenLetter:
+            seenLetter += char
+            pattern += str(count)
+            count += 1
+        else:
+            pattern += str(seenLetter.index(char))
+
+    return pattern
+
+
+main("dicts")
