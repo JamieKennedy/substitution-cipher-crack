b6632fd1320a6dbf1a977f76299ca46ebf69b538
Jamie Kennedy <jamiekennedy97@gmail.com>
2019-02-09 13:24:37 +0000

    Seperated dictionaries
    Seperated the single dictionary into dictioaries of distinct lengths

---
+++ seperateDictionary.py
@@ -0,0 +1,28 @@
+
+
+def main():
+    wordsSep = {}
+    with open("dicts/dictionaryAll.txt") as dictionary:
+        words = dictionary.readlines()
+
+    for word in words:
+        word = word.replace("\n", "")
+        if len(word) not in wordsSep:
+            wordsSep[len(word)] = [word]
+        else:
+            wordsSep[len(word)].append(word)
+
+    for key in wordsSep:
+        words = wordsSep[key]
+        data = "".join(word + "\n" for word in words)
+        fileName = "dicts/dictionaryLen" + str(key) + ".txt"
+        writeFile(fileName, data)
+
+
+def writeFile(fileName, data):
+    file = open(fileName, "w")
+    file.write(data)
+    file.close()
+
+
+main()
